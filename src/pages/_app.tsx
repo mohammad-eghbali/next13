c28b8b92996872176a211645a20f6953a358118d
mohammad-eghbali <mohamad68@gmail.com>
2023-02-07 11:51:05 +0330

    add gtm script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-DD869VZF1Y"
+      />
+      <Script
+        id="show-banner"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `  window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-DD869VZF1Y');`,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
