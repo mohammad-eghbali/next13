2ccfdbe2297e9827996602b84e079c2bd8bad717
mohammad-eghbali <mohamad68@gmail.com>
2023-02-07 14:05:03 +0330

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
-// import { Partytown } from "@builder.io/partytown/react";
-// import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* <Head>
-        <Partytown debug={true} forward={["dataLayer.push"]} />
-      </Head> */}
-
       <Script
+        async
         src="https://www.googletagmanager.com/gtag/js?id=G-DD869VZF1Y"
-        strategy="afterInteractive"
       />
       <Script
         id="show-banner"
