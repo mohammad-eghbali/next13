ae72ae3826b4543425bbc322d7fc3a67de36abaf
mohammad-eghbali <mohamad68@gmail.com>
2023-02-07 13:26:19 +0330

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
-import { Partytown } from "@builder.io/partytown/react";
-import Head from "next/head";
+// import { Partytown } from "@builder.io/partytown/react";
+// import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
+      {/* <Head>
         <Partytown debug={true} forward={["dataLayer.push"]} />
-      </Head>
+      </Head> */}
 
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-DD869VZF1Y"
