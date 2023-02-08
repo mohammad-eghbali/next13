8afe0b74e226dd3411ab9823161a5f5d9b650d28
mohammad-eghbali <mohamad68@gmail.com>
2023-02-08 10:57:11 +0330

    test

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,11 +1,14 @@
+import Head from "next/head";
 import Link from "next/link";
 import React from "react";
 
-type Props = {};
-
-const TestPage = (props: Props) => {
+const TestPage = () => {
   return (
     <div>
+      <Head>
+        <title>Test Page 1</title>
+      </Head>
+
       <h1>PAGE TEST1</h1>
       <h2>page</h2>
       <Link href="/">back</Link>
